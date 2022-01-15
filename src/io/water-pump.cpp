8acc42b6875162423be5fb37d44fda709fe079bd
Eduardo Ferreira <eduardoofe@gmail.com>
2022-01-15 14:50:11 -0300

    Add water pump io

---
+++ src/io/water-pump.cpp
@@ -0,0 +1,18 @@
+#include "water-pump.h"
+
+#include "../config.h"
+
+#include <Arduino.h>
+
+void WaterPump::setup() {
+  pinMode(WATER_PUMP_DIGITAL_PIN, OUTPUT);
+}
+
+void WaterPump::turnOn() {
+  digitalWrite(WATER_PUMP_DIGITAL_PIN, HIGH);
+}
+
+void WaterPump::turnOff() {
+  digitalWrite(WATER_PUMP_DIGITAL_PIN, LOW);
+}
+
---
+++ src/io/water-pump.h
@@ -0,0 +1,13 @@
+#ifndef WATER_PUMP_HEADER 
+#define WATER_PUMP_HEADER
+
+class WaterPump {
+
+public:
+  void setup();
+  void turnOn();
+  void turnOff();
+};
+
+#endif
+

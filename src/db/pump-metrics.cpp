82832dbc48f80a7f457987ddae190a328829089f
Eduardo Ferreira <eduardoofe@gmail.com>
2022-01-18 21:35:05 -0300

    Save a PumpMetric on SD card

--- src/db/pump-metrics.cpp
+++ src/db/pump-metrics.cpp
@@ -1,12 +1,13 @@
 #include "pump-metrics.h"
 
-#include <SPI.h>
+#include <RTClib.h> // Include DateTime
 #include <SD.h>
-#include <RTClib.h>  // Include DateTime
+#include <SPI.h>
 
 #include "../model/pump-metrics.h"
 
 #define DATETIME_FORMAT "YYYY-MM-DDThh:mm:ss"
+#define FILE_NAME "pumpmet.txt"
 
 void PumpMetricsDB::setup() {
   pinMode(53, OUTPUT);
@@ -18,33 +19,28 @@ void PumpMetricsDB::setup() {
 }
 
 void PumpMetricsDB::save(PumpMetrics &p) {
-  //File json = SD.open("pump-metrics.json", FILE_WRITE);
-  File json = SD.open("write.txt", FILE_WRITE);
+  SD.remove(FILE_NAME);
 
-  if (json) {
-    StaticJsonDocument<200> doc;    
+  File file = SD.open("pumpmet.txt", FILE_WRITE);
+
+  if (file) {
+    StaticJsonDocument<200> doc;
 
     char *turnedOnAt = p.getTurnedOnAt().toString(DATETIME_FORMAT);
-
-    Serial.println(turnedOnAt);
-
-    doc["turnedOnAt"] = turnedOnAt; 
+    doc["turnedOnAt"] = turnedOnAt;
 
     if (p.getTurnedOffAt()) {
       char *turnedOffAt = p.getTurnedOffAt()->toString(DATETIME_FORMAT);
 
-      doc["turnedOffAt"] = turnedOffAt; 
+      doc["turnedOffAt"] = turnedOffAt;
     }
 
     char content[200];
     serializeJsonPretty(doc, content);
 
-    Serial.println(content);
-
-    json.println(content);
-    json.close();
+    file.println(content);
+    file.close();
   } else {
     Serial.println("Error: PumpMetricsDB save failed");
   }
 }
-
